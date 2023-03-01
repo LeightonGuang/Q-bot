d4ce45f8f3e2b68221b92a8218d57cd382d0ffd4
LeightonGuang <leighton.guang@gmail.com>
2023-03-01 10:50:53 +0800

    updated ping command

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
       .setTitle("HELP")
       .setDescription("list of commands available for Q bot\n vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
       .addFields(
-        { name: "/online", value: "Bot online status" },
+        { name: "/ping", value: "ping the bot to check online status" },
         { name: "/setup", value: "Create roles for queue" },
         { name: "/creator", value: "Creator of Q Bot" },
         { name: "/player-profile", value: "Setup player information" },
