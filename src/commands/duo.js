0ade31b45a5651352c7495d35d88ed0a08790a8b
LeightonGuang <leighton.guang@gmail.com>
2023-03-03 00:50:26 +0800

    moved vc creation and use writeToFile func

--- src/commands/duo.js
+++ src/commands/duo.js
@@ -1,7 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
-const Discord = require('discord.js');
-const fs = require('node:fs');
-const globalFunctions = require('../globalFunctions.js');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const fs = require("node:fs");
+const writeToFile = require('../utils/writeToFile');
 
 let categoryId = "1074976911312289862";
 
@@ -9,58 +8,54 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("duo")
     .setDescription("Select someone to duo with")
-    .addUserOption((option) =>
-      option
-        .setName('duo')
-        .setDescription('duo partner')
-        .setRequired(true)),
+    .addUserOption((option) => option.setName("duo").setDescription("duo partner").setRequired(true)),
 
   async execute(interaction) {
     const { member, guild } = interaction;
-    //voiceChannel is id of queue waiting room
-    let member1 = member;
-    let member2 = interaction.options.getMember('duo');
 
-    let queueWaitingRoomId = guild.channels.cache.find(channel => channel.name === "queue waiting room");
+    let member1 = member;
+    let member2 = interaction.options.getMember("duo");
+
+    let queueWaitingRoomId = guild.channels.cache.find((channel) => channel.name === "queue waiting room");
+
+    let queueNotificationChannel = guild.channels.cache.find(
+      (c) => c.name === "queue-notification"
+    );
+
+    const inviteEmbed = new EmbedBuilder()
+      .setTitle("Private Duo VC invite")
+      .setDescription(`${member2}, you got an invited from ${member1}`)
+      .setTimestamp()
+      .setColor("0x00FF00");
+
+    const inviteRow = new ActionRowBuilder().setComponents(
+      new ButtonBuilder()
+        .setCustomId("accept")
+        .setLabel("Accept")
+        .setStyle(ButtonStyle.Success),
+      new ButtonBuilder().setCustomId("decline")
+        .setLabel("Decline")
+        .setStyle(ButtonStyle.Danger),
+    );
 
     let hasMember1 = queueWaitingRoomId.members.has(member1.id);
     let hasMember2 = queueWaitingRoomId.members.has(member2.id);
 
     /*if the person who used the command and the targeted member is in queue waiting room*/
     if (hasMember1 && hasMember2) {
-      let newDuoVoiceChannel = await guild.channels.create({
-        name: member1.user.username + "'s duo vc",
-        type: 2,
-        userLimit: 2,
-        parent: categoryId,
-        permissionOverwrites: [
-          {
-            id: guild.id,
-            deny: [Discord.PermissionsBitField.Flags.Connect],
-          },
-          {
-            id: member1,
-            allow: [Discord.PermissionsBitField.Flags.Connect],
-          },
-          {
-            id: member2,
-            allow: [Discord.PermissionsBitField.Flags.Connect],
-          }
-        ]
-      })
-      let newDuoObj = guild.channels.cache.find(channel => channel.name === member1.user.username + "'s duo vc");
-      member1.voice.setChannel(newDuoObj);
-      member2.voice.setChannel(newDuoObj);
-      interaction.reply({ content: `${member1.user.username} and ${member2.user.username} moved to ${member1.user.username + "'s duo vc"}`, ephemeral: true });
-      console.log("LOG: " + `${member1.user.username + "'s duo vc"} created`)
-
+      //write this to vcInvite
+      //duo is to identify its duo for handler and last item is counter for how many accept
       let dataFile = fs.readFileSync('data.json');
       let dataObj = JSON.parse(dataFile);
-      let customVoiceChannel = dataObj.customVoiceChannel;
+      let vcInvite = dataObj.vcInvite;
 
-      customVoiceChannel.push(member1.user.username + "'s duo vc");
-      globalFunctions.writeToFile(dataObj, 'data.json');
+      let duoVcList = ["duo", member1.id, member2.id, false];
 
+      vcInvite.push(duoVcList);
+      writeToFile(dataObj, 'data.json');
+
+      await interaction.reply({ content: "invite sent to " + member2.user.username, ephemeral: true });
+      queueNotificationChannel.send({ content: `${member2}, you got an invite from ${member1} to private duo vc`, components: [inviteRow] });
 
     } else {
       await interaction.reply({ content: "Some members are not in queue waiting room", ephemeral: true });
