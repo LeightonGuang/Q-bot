c2dbf8507141827a7dae8716bcfa79746c2b6f5c
LeightonGuang <leighton.guang@gmail.com>
2023-03-12 18:05:59 +0800

    trio private vc is seperated

--- src/commands/trio.js
+++ src/commands/trio.js
@@ -1,82 +1,84 @@
-const { SlashCommandBuilder } = require('discord.js');
-const Discord = require('discord.js');
-const fs = require('node:fs');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const fs = require("node:fs");
 const writeToFile = require('../utils/writeToFile');
-
-let categoryId = "1074976911312289862";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("trio")
-    .setDescription("Select someone two people to trio with")
-
+    .setDescription("Select someone to trio with")
     .addUserOption((option) =>
       option
-        .setName('member2')
-        .setDescription('trio partner 1')
+        .setName("trio1")
+        .setDescription("trio1")
         .setRequired(true)
     )
-
     .addUserOption((option) =>
       option
-        .setName('member3')
-        .setDescription('trio partner 2')
+        .setName("trio2")
+        .setDescription("trio2")
         .setRequired(true)
     ),
 
   async execute(interaction) {
     const { member, guild } = interaction;
-    //voiceChannel is id of queue waiting room
-    let member1 = member;
-    let member2 = interaction.options.getMember('member2');
-    let member3 = interaction.options.getMember('member3')
 
-    let queueWaitingRoomId = guild.channels.cache.find(channel => channel.name === "queue waiting room");
+    let member1 = member;
+    let member2 = interaction.options.getMember("trio1");
+    let member3 = interaction.options.getMember("trio2");
+
+    let queueWaitingRoomId = guild.channels.cache.find((channel) => channel.name === "queue waiting room");
+
+    let queueNotificationChannel = guild.channels.cache.find(
+      (c) => c.name === "queue-notification"
+    );
+
+    const inviteEmbed = new EmbedBuilder()
+      .setTitle("Private Trio VC invite")
+      .setDescription(`${member2} and ${member3}, you got an invited from ${member1}`)
+      .setTimestamp()
+      .setColor("0x00FF00");
+
+    const inviteRow = new ActionRowBuilder()
+      .setComponents(
+        new ButtonBuilder()
+          .setCustomId("accept")
+          .setLabel("Accept")
+          .setStyle(ButtonStyle.Success),
+
+        new ButtonBuilder()
+          .setCustomId("decline")
+          .setLabel("Decline")
+          .setStyle(ButtonStyle.Danger),
+      );
 
     let hasMember1 = queueWaitingRoomId.members.has(member1.id);
     let hasMember2 = queueWaitingRoomId.members.has(member2.id);
     let hasMember3 = queueWaitingRoomId.members.has(member3.id);
 
+    //check if member have already sent an invite
+
     /*if the person who used the command and the targeted member is in queue waiting room*/
     if (hasMember1 && hasMember2 && hasMember3) {
-      let newTrioVoiceChannel = await guild.channels.create({
-        name: member1.user.username + "'s trio vc",
-        type: 2,
-        userLimit: 3,
-        parent: categoryId,
-        permissionOverwrites: [
-          {
-            id: guild.id,
-            deny: [Discord.PermissionsBitField.Flags.Connect],
-          },
-          {
-            id: member1,
-            allow: [Discord.PermissionsBitField.Flags.Connect],
-          },
-          {
-            id: member2,
-            allow: [Discord.PermissionsBitField.Flags.Connect],
-          },
-          {
-            id: member3,
-            allow: [Discord.PermissionsBitField.Flags.Connect],
-          }
-        ]
-      })
-      let newTrioObj = guild.channels.cache.find(channel => channel.name === member1.user.username + "'s trio vc");
-      member1.voice.setChannel(newTrioObj);
-      member2.voice.setChannel(newTrioObj);
-      member3.voice.setChannel(newTrioObj);
-      interaction.reply({ content: `${member1.user.username}, ${member2.user.username} and ${member3.user.username} moved to ${member1.user.username + "'s trio vc"}`, ephemeral: true });
-      console.log("LOG: " + `${member1.user.username + "'s trio vc"} created`)
-
+      //write this to vcInvite
+      //trio is to identify its trio for handler and last item is counter for how many accept
       let dataFile = fs.readFileSync('data.json');
       let dataObj = JSON.parse(dataFile);
-      let customVoiceChannel = dataObj.customVoiceChannel;
+      let vcInvite = dataObj.vcInvite;
 
-      customVoiceChannel.push(member1.user.username + "'s trio vc");
-      writeToFile(dataObj, 'data.json');
+      await interaction.reply({ content: `invite sent to ${member2.user.username} and ${member3.user.username}`, ephemeral: true });
+      queueNotificationChannel.send({ content: `${member2} and ${member3}, you got an invited from ${member1} to a private trio vc.`, components: [inviteRow] })
+        .then(interaction => {
+          console.log("interaction.id: " + interaction.id);
+          let trioVcObj = {
+            vcType: "trio",
+            inviteList: [member1.id, member2.id, member3.id],
+            interactionId: interaction.id,
+            decision: [true, 0, 0]
+          };
 
+          vcInvite.push(trioVcObj);
+          writeToFile(dataObj, 'data.json');
+        })
 
     } else {
       await interaction.reply({ content: "Some members are not in queue waiting room", ephemeral: true });
---
+++ src/handlers/privateVc/newPrivateTrioVc.js
@@ -0,0 +1,147 @@
+const fs = require("node:fs");
+const Discord = require("discord.js");
+const writeToFile = require("../../utils/writeToFile");
+
+module.exports = (client) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (interaction.isButton()) {
+      const { guild } = interaction;
+
+      let dataFile = fs.readFileSync("data.json");
+      let dataObj = JSON.parse(dataFile);
+
+      let vcInvite = dataObj.vcInvite;
+      let buttonClicked = interaction.customId;
+
+      let invited = false;
+      let categoryId = "1074976911312289862";
+      let vcType, memberPos;
+
+      //check if the member interacted is invited
+      for (let invite of vcInvite) {
+        //if member interacted with their own invite
+        if (invite.inviteList[0] === interaction.user.id) {
+          try {
+            await interaction.reply({ content: "You can't accept or decline your own invite", ephemeral: true });
+            console.log("LOG: \t" + "member interacted with their own invite");
+          } finally {
+            return 1;
+          }
+
+        }
+        for (let i = 1; i < invite.inviteList.length; i++) {
+          let member = invite.inviteList[i];
+          if (interaction.user.id === member) {
+            vcType = invite.vcType;
+            memberPos = i;
+            invited = true;
+            break;
+          }
+        }
+      }
+      //if member is in the inviteList
+      if (invited && vcType === "trio") {
+        console.log("running trio code");
+        for (let i = 0; i < vcInvite.length; i++) {
+          let invite = vcInvite[i];
+          //if interactionId is the same as interaction.message.id
+          if (invite.interactionId === interaction.message.id) {
+
+            //check the vcType in that object
+            if (invite.vcType === "trio") {
+              //if the vcType is "trio" then run then keep going
+              if (buttonClicked === "accept") {
+                await interaction.reply({ content: "You have accepted the invite", ephemeral: true });
+
+                //add 1 to decision
+                dataObj.vcInvite[i].decision[memberPos] = true;
+                writeToFile(dataObj, "data.json");
+
+                //if all the member accepted their invite
+                if (dataObj.vcInvite[i].decision.every(Boolean)) {
+
+                  //create new private vc
+                  let member1 = invite.inviteList[0];
+                  let member2 = invite.inviteList[1];
+                  let member3 = invite.inviteList[2];
+                  member1 = guild.members.cache.get(member1);
+                  member2 = guild.members.cache.get(member2);
+                  member3 = guild.members.cache.get(member3);
+
+                  //make private vc
+                  let createTrioVc = await guild.channels.create({
+                    name: member1.user.username + "'s trio vc",
+                    type: 2,
+                    userLimit: invite.inviteList.length,
+                    parent: categoryId,
+                    permissionOverwrites: [
+                      {
+                        id: guild.id,
+                        deny: [Discord.PermissionsBitField.Flags.Connect],
+                      },
+                      {
+                        id: member1,
+                        allow: [Discord.PermissionsBitField.Flags.Connect],
+                      },
+                      {
+                        id: member2,
+                        allow: [Discord.PermissionsBitField.Flags.Connect],
+                      },
+                      {
+                        id: member3,
+                        allow: [Discord.PermissionsBitField.Flags.Connect],
+                      },
+                    ],
+                  });
+
+                  let newTrioObj = guild.channels.cache.find((channel) => channel.name === member1.user.username + "'s trio vc");
+                  member1.voice.setChannel(newTrioObj);
+                  member2.voice.setChannel(newTrioObj);
+                  member3.voice.setChannel(newTrioObj);
+
+                  //delete message and button in queue-notification
+                  let interactionId = dataObj.interactionId;
+                  interaction.message.delete(interactionId);
+
+                  //delete invite from vcInvite
+                  dataObj.vcInvite = vcInvite.filter((item) => item !== invite);
+                  writeToFile(dataObj, "data.json");
+
+                  //add private vc name in cusotmVoiceChannel
+                  let customVoiceChannel = dataObj.customVoiceChannel;
+                  customVoiceChannel.push(member1.user.username + "'s trio vc");
+                  writeToFile(dataObj, "data.json");
+                }
+
+              } else if (buttonClicked === "decline") {
+                await interaction.reply({ content: `You declined the invite`, ephemeral: true });
+                console.log("member declined the invite");
+                //delete the invite from vcInvite
+                dataObj.vcInvite = vcInvite.filter((item) => item !== invite);
+                writeToFile(dataObj, "data.json");
+
+                //delete message and button in queue-notification
+                let interactionId = dataObj.interactionId;
+                interaction.message.delete(interactionId);
+              }
+              break;
+            }
+            //else do nothing
+          }
+        }
+
+      } else {
+        try {
+          await interaction.reply({ content: "You are not invited", ephemeral: true });
+          console.log("LOG: \t" + "member interacted is not invited");
+        } catch (error) {
+          return 1;
+        }
+      }
+      /*
+        loop through all the object in vcInvite and check if the interactionId
+        matches interaction.id
+      */
+    }
+  });
+};
