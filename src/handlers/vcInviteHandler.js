db71244164231952f8a7569c650a0fc5ca858244
LeightonGuang <leighton.guang@gmail.com>
2023-03-12 18:06:20 +0800

    trio private vc

--- src/handlers/vcInviteHandler.js
+++ src/handlers/vcInviteHandler.js
@@ -1,4 +1,7 @@
 module.exports = (client) => {
   let newPrivateDuoVc = require("../handlers/privateVc/newPrivateDuoVc");
   newPrivateDuoVc(client);
+
+  let newPrivateTrioVc = require("../handlers/privateVc/newPrivateTrioVc");
+  newPrivateTrioVc(client);
 };
