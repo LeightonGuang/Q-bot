60a91ec55031e71cc0caa58770a0c6e3777a3c95
LeightonGuang <leighton.guang@gmail.com>
2023-05-05 16:19:04 +0800

    buttonHandler.js

--- src/handlers/buttonHandler.js
+++ src/handlers/buttonHandler.js
@@ -3,30 +3,22 @@ const writeToFile = require("../utils/writeToFile");
 const updateQueueEmbed = require("../utils/updateQueueEmbed");
 
 /**
- * when any queue button is used
+ * if a button is clicked
  * 
- * add the member the the corresponding queue
- * update the embed
- * check if any member matched
- * region
- * rank
- * if they match start a game
- * 
- * when dequeue is pressed
- * check if member is in any queue
- * if member is in queue (check queue by check if they have role)
- * remove them from their role and their id in the list
- * 
- * if not in queue then reply member not in queue
+ * run the functions
  */
 
 module.exports = (client) => {
   client.on("interactionCreate", async (interaction) => {
-    let queueButtonHandler = require("./queueButtonHandler");
-    queueButtonHandler(interaction);
+    console.log("FILE: \t" + "buttonHandler.js");
+    if (interaction.isButton()) {
 
-    let pollButton = require("../event/pollButtons");
-    pollButton(interaction);
+      let queueButtonHandler = require("./queueButtonHandler");
+      queueButtonHandler(interaction);
 
+      let pollButton = require("../event/pollButtons");
+      pollButton(interaction);
+
+    }//not a button press
   });
 };
