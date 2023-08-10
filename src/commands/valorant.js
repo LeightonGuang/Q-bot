60420245f43a26482d21dd542dc85dff27cbe65d
LeightonGuang <leighton.guang@gmail.com>
2023-08-10 22:41:10 +0800

    last game player embed with stats working

--- src/commands/valorant.js
+++ src/commands/valorant.js
@@ -720,13 +720,9 @@ module.exports = {
       await interaction.reply("Loading info...");
 
       let userId = interaction.user.id;
-
       let userObj = dataObj.playerList.find(obj => obj.id === userId);
-
       let accountObj = userObj.riotAccountList.find(obj => obj.active === true);
-
       let riotId = accountObj.riotId;
-
       let trackerProfileUrl = profileUrl(riotId);
 
       const browser = await puppeteer.launch({ headless: false });
@@ -735,7 +731,6 @@ module.exports = {
       await page.setViewport({ width: 1920, height: 1080 });
       await page.goto(trackerProfileUrl);
       await page.waitForSelector(".vmr");
-
       //first game on the list
       const lastGameSelector = "#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.area-main > div.area-matches.flex.flex-col.gap-4 > div.matches > div.trn-gamereport-list.trn-gamereport-list--compact > div:nth-child(1) > div.trn-gamereport-list__group-entries > div:nth-child(1)";
 
@@ -743,38 +738,64 @@ module.exports = {
 
       await page.waitForSelector(".st-content__item .trn-ign .trn-ign__username");
 
-      const allPlayerStats = await page.evaluate(() => {
-        const allStats = Array.from(document.querySelectorAll('.scoreboard .st-content__item')).flatMap(item => Array.from(item.querySelectorAll('.value'))).map(element => element.textContent);
-        return allStats;
-      });
-
-      console.log(allPlayerStats);
-
       //allPlayerInfo is a list of object with player stats 
       const allPlayerInfo = await page.evaluate(() => {
+        const allPlayerStats = Array.from(document.querySelectorAll('.scoreboard .st-content__item')).flatMap(item => Array.from(item.querySelectorAll('.value'))).map(element => element.textContent);
         const playerInfo = Array.from(document.querySelectorAll(".vm-table .st-content__item"));
 
-        let playerNum = 1;
-
-        const stats = playerInfo.map((player) => ({
+        const stats = playerInfo.map((player, playerNum) => ({
           riotName: player.querySelector(".st-content__item .trn-ign .trn-ign__username").textContent.trim(),
           riotId: player.querySelector(".st-content__item .trn-ign .trn-ign__discriminator").textContent.trim(),
           agentIconUrl: player.querySelector(".scoreboard .st-content__item .st-custom-name .image img").getAttribute("src"),
           rank: player.querySelector(".scoreboard .st-content__item .info .rank span").textContent,
           rankIconUrl: player.querySelector(".scoreboard .st-content__item .st__item--align-center .image img").getAttribute("src"),
-          acs: allPlayerStats[1 * playerNum],
-          kill: allPlayerStats[2 * playerNum],
-          death: allPlayerStats[3 * playerNum],
-          assist: allPlayerStats[4 * playerNum]
+          acs: allPlayerStats[(playerNum * 14) + 1],
+          kill: allPlayerStats[(playerNum * 14) + 2],
+          death: allPlayerStats[(playerNum * 14) + 3],
+          assist: allPlayerStats[(playerNum * 14) + 4],
+          kd: allPlayerStats[(playerNum * 14) + 6],
+          adr: allPlayerStats[(playerNum * 14) + 8],
+          hs: allPlayerStats[(playerNum * 14) + 9],
+          fk: allPlayerStats[(playerNum * 14) + 11],
+          fd: allPlayerStats[(playerNum * 14) + 12]
         }));
         return stats;
       });
 
       console.log(allPlayerInfo);
 
+      let embedList = []
       for (let i = 0; i < 10; i++) {
+        let playerObj = allPlayerInfo[i];
 
+        let embedColour;
+        if (i < 5) {
+          embedColour = 0x49c6b8;
+
+        } else if (i > 4) {
+          embedColour = 0xb95564;
+        }
+
+        let playerEmbed = new EmbedBuilder()
+          .setColor(embedColour)
+          .setAuthor({ name: playerObj.riotName + playerObj.riotId, iconURL: playerObj.agentIconUrl })
+          .setThumbnail(playerObj.rankIconUrl)
+          .addFields([
+            { name: "Kills: ", value: playerObj.kill, inline: true },
+            { name: "Deaths:", value: playerObj.kill, inline: true },
+            { name: "Assists:", value: playerObj.kill, inline: true },
+            { name: "ACS:    ", value: playerObj.acs, inline: true },
+            { name: "K/D:", value: playerObj.kd, inline: true },
+            { name: "Headshot%:", value: playerObj.hs, inline: true },
+            { name: "ADR: ", value: playerObj.adr, inline: true },
+            { name: "First Kill:", value: playerObj.fk, inline: true },
+            { name: "First Death:", value: playerObj.fd, inline: true }
+          ])
+
+        embedList.push(playerEmbed);
       }
+
+      interaction.editReply({ content: "", embeds: embedList });
 
       await page.screenshot({ path: "screenshot.png", fullPage: true });
       console.log("LOG: \t" + "screenshot");
