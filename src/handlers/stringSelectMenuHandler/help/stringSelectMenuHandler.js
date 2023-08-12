7e7afa511dc53e9457777eaca27d4022bf36d7bd
LeightonGuang <leighton.guang@gmail.com>
2023-08-12 21:10:20 +0800

    made sub command bold and italic

--- src/handlers/stringSelectMenuHandler/help/stringSelectMenuHandler.js
+++ src/handlers/stringSelectMenuHandler/help/stringSelectMenuHandler.js
@@ -15,7 +15,7 @@ module.exports = (client) => {
           .setColor(0xffffff)
           .setAuthor({ name: "Q bot" })
           .setTitle("/help")
-          .setDescription("List of commands available for Q bot")
+          .setDescription("All the commands that are available to use in the server")
           .addFields(
             { name: "/ping", value: "Ping the bot to check online status" },
             { name: "/setup", value: "Create roles for queue" },
@@ -33,15 +33,15 @@ module.exports = (client) => {
         const accountEmbed = new EmbedBuilder()
           .setColor(0xFFFF00)
           .setAuthor({ name: "Q bot" })
-          .setTitle("/account [sub command]")
+          .setTitle("/account ***sub-command***")
           .setDescription("List of all sub commands available for /account")
           .addFields(
-            { name: "add-riot-account", value: "Add a riot account with riot id, rank and region." },
-            { name: "add-steam-account", value: "Add a steam account with account name, friend code and profile url." },
-            { name: "edit-riot-account", value: "Edit your riot id, rank, region." },
-            { name: "edit-steam-account", value: "Edit your account name, friend code and profile url." },
-            { name: "list-all", value: "List al the accounts you have added to Qs" },
-            { name: "select", value: "If you have multiple accounts, you can select which account to queue with." }
+            { name: "***add-riot-account***", value: "Add a riot account with riot id, rank and region." },
+            { name: "***add-steam-account***", value: "Add a steam account with account name, friend code and profile url." },
+            { name: "***edit-riot-account***", value: "Edit your riot id, rank, region." },
+            { name: "***edit-steam-account***", value: "Edit your account name, friend code and profile url." },
+            { name: "***list-all***", value: "List al the accounts you have added to Qs" },
+            { name: "***select***", value: "If you have multiple accounts, you can select which account to queue with." }
           )
           .setTimestamp()
 
@@ -53,12 +53,14 @@ module.exports = (client) => {
         const valorantEmbed = new EmbedBuilder()
           .setColor(0xFF4553)
           .setAuthor({ name: "Q bot" })
-          .setTitle("/valorant [sub command]")
+          .setTitle("/valorant ***sub-command***")
           .setDescription("List of all sub commands availble for /valorant")
           .addFields(
-            { name: "check-rank", value: "Check current rank and peak rank" },
-            { name: "last-game-stats", value: "Check a player's last game stats" },
-            { name: "win-percentage", value: "Check your current act rank win percentage" }
+            { name: "***check-rank***", value: "Check current rank and peak rank" },
+            { name: "***last-game-stats***", value: "Check a player's last game stats" },
+            { name: "***win-percentage***", value: "Check your current act rank win percentage" },
+            { name: "***upcoming-events***", value: "Upcoming Valorant Champions Tour events" },
+            { name: "***ongoing-events***", value: "Ongoing Valorant Champions Tour events" }
           )
           .setTimestamp()
 
