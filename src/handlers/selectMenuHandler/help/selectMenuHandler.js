849d1c0f1c8c2c047e30cbff6b859f538ca85562
LeightonGuang <leighton.guang@gmail.com>
2023-08-12 16:47:27 +0800

    new select menu handler

---
+++ src/handlers/selectMenuHandler/help/selectMenuHandler.js
@@ -0,0 +1,70 @@
+const { EmbedBuilder } = require("discord.js");
+module.exports = (client) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isStringSelectMenu()) return;
+    const splittedArray = interaction.customId.split('-');
+
+    //if it is not a help select menu then end this code file
+    if (splittedArray[0] !== "help") return;
+
+    let menuSubCommand = interaction.values[0];
+
+    switch (menuSubCommand) {
+      case "help":
+        const helpEmbed = new EmbedBuilder()
+          .setColor(0xffffff)
+          .setAuthor({ name: "Q bot" })
+          .setTitle("HELP")
+          .setDescription("List of commands available for Q bot")
+          .addFields(
+            { name: "/ping", value: "Ping the bot to check online status" },
+            { name: "/setup", value: "Create roles for queue" },
+            { name: "/duo, /trio, /quad, /stack ", value: "Creates a private vc for you and the people you want" }
+          )
+          .setTimestamp()
+
+        interaction.message.edit({ embeds: [helpEmbed] });
+        await interaction.deferUpdate();
+        break;
+
+      case "account":
+
+
+        const accountEmbed = new EmbedBuilder()
+          .setColor(0xFFFF00)
+          .setAuthor({ name: "Q bot" })
+          .setTitle("/account [sub command]")
+          .setDescription("List of all sub commands available for /account")
+          .addFields(
+            { name: "add-riot-account", value: "Add a riot account with riot id, rank and region." },
+            { name: "add-steam-account", value: "Add a steam account with account name, friend code and profile url." },
+            { name: "edit-riot-account", value: "Edit your riot id, rank, region." },
+            { name: "edit-steam-account", value: "Edit your account name, friend code and profile url." },
+            { name: "list-all", value: "List al the accounts you have added to Qs" },
+            { name: "select", value: "If you have multiple accounts, you can select which account to queue with." }
+          )
+          .setTimestamp()
+
+        interaction.message.edit({ embeds: [accountEmbed] });
+        await interaction.deferUpdate();
+        break;
+
+      case "valorant":
+        const valorantEmbed = new EmbedBuilder()
+          .setColor(0xFF4553)
+          .setAuthor({ name: "Q bot" })
+          .setTitle("/valorant [sub command]")
+          .setDescription("List of all sub commands availble for /valorant")
+          .addFields(
+            { name: "check-rank", value: "Check current rank and peak rank" },
+            { name: "last-game-stats", value: "Check a player's last game stats" },
+            { name: "win-percentage", value: "Check your current act rank win percentage" }
+          )
+          .setTimestamp()
+
+        interaction.message.edit({ embeds: [valorantEmbed] });
+        await interaction.deferUpdate();
+        break;
+    }
+  })
+}
