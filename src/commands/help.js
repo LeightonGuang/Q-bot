201fa6da2870bd1ffc076b469b65966bf0169ed6
LeightonGuang <leighton.guang@gmail.com>
2023-08-12 16:46:33 +0800

    updated new /help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,25 +1,38 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("list of all commands"),
+    .setDescription("list of all commands")
+  ,
+
   async execute(interaction) {
     const helpEmbed = new EmbedBuilder()
+      .setColor(0xffffff)
       .setAuthor({ name: "Q bot" })
       .setTitle("HELP")
-      .setDescription("list of commands available for Q bot\n vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+      .setDescription("list of commands available for Q bot")
       .addFields(
-        { name: "/ping", value: "ping the bot to check online status" },
+        { name: "/ping", value: "Ping the bot to check online status" },
         { name: "/setup", value: "Create roles for queue" },
         { name: "/creator", value: "Creator of Q Bot" },
-        { name: "/account", value: "Setup player account info" },
         { name: "/duo, /trio, /quad, /stack ", value: "Creates a private vc for you and the people you want" }
       )
       .setTimestamp()
-      .setFooter({ text: "Q bot" })
-      .setColor(0xFFFFFF)
-    await interaction.reply({ embeds: [helpEmbed] });
+
+    const replyObj = await interaction.reply({ embeds: [helpEmbed], fetchReply: true });
+
+    const helpSelectMenu = new ActionRowBuilder().setComponents(
+      new StringSelectMenuBuilder().setCustomId(`help-${replyObj.id}`).setOptions([
+        { label: "/help", value: "help" },
+        { label: "/account", value: "account" },
+        { label: "/valorant", value: "valorant" }
+      ])
+    );
+
+    interaction.editReply({ components: [helpSelectMenu.toJSON()] });
+
+
     console.log("LOG: \t embed help list");
   },
 };
