37a75828568b4c461119911225d71a75fe777547
LeightonGuang <leighton.guang@gmail.com>
2023-08-28 04:30:54 +0800

    upcoming-events sub command working

---
+++ src/sub-commands/valorant/upcoming-events.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = async (interaction) => {
+  let year = new Date().getFullYear();
+  let vlr_url = "https://vlr.gg";
+
+  let valorantEventEmbedList = [];
+  let upcomingEventList = [];
+
+  const fetchEvents = require("../../utils/valorant/fetchEvents");
+  const fetchEventMatch = require("../../utils/valorant/fetchEventMatch");
+  const sendEmbed = require("../../utils/valorant/sendEmbed");
+
+  let upcomingEventEmbedHeader = new EmbedBuilder()
+    .setColor(0xffffff)
+    .setTitle("Upcoming Valorant Champions Tour " + year)
+    .setURL(vlr_url + "/vct-" + year)
+    .setDescription("Riot's official " + year + " Valorant tournament circuit");
+
+  valorantEventEmbedList.push(upcomingEventEmbedHeader);
+
+  await interaction.reply({
+    embeds: valorantEventEmbedList,
+    fetchReply: true,
+  });
+
+  await fetchEvents(upcomingEventList);
+  await fetchEventMatch(upcomingEventList);
+  await sendEmbed(upcomingEventList);
+};
