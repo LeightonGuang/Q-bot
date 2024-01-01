5e1ad8d3d70934829ba0b81334477988ff9bcd6e
LeightonGuang <leighton.guang@icloud.com>
2024-01-01 15:12:36 +0000

    feat: convert riot id to url link

--- src/utils/valorant/profileUrl.js
+++ src/utils/valorant/profileUrl.js
@@ -3,7 +3,6 @@ module.exports = (riotId) => {
   let modifiedId = riotId.replace(/\s+/g, "%20");
   modifiedId = modifiedId.replace(/#/g, "%23");
 
-  let playerUrl =
-    "https://tracker.gg/valorant/profile/riot/" + modifiedId + "/overview";
+  let playerUrl = "https://tracker.gg/valorant/profile/riot/" + modifiedId;
   return playerUrl;
 };
