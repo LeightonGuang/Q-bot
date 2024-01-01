6a59ebc592d1f060488084b7541f991aed86a8b3
LeightonGuang <leighton.guang@icloud.com>
2024-01-01 15:06:26 +0000

    feat: get csgo tier one events

--- src/commands/csgo-event.js
+++ src/commands/csgo-event.js
@@ -3,12 +3,22 @@ const fs = require('fs');
 const writeToFile = require('../utils/writeToFile');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { on } = require('events');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("csgo-event")
-    .setDescription("get any tier 1 csgo event "),
+    .setDescription("get any tier 1 csgo event ")
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName("ongoing-events")
+        .setDescription("csgo events that are ongoing right now.")
+    )
+
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName("upcoming-events")
+        .setDescription("Upcoming csgo events.")
+    ),
 
   async execute(interaction) {
 
@@ -19,51 +29,70 @@ module.exports = {
 
     const { guild, channel } = interaction;
 
-    let url = "https://hltv.org"
+    let url = "https://hltv.org";
     let dataFile = fs.readFileSync('data.json');
 
-    //const event = new GuildScheduledEvent();
-    let ongoingEventEmbedList = [];
+    let subCommand = interaction.options.getSubcommand();
 
-    let ongoingEventHeaderEmbed = new EmbedBuilder()
-      .setColor(0xFF0000)
-      .setTitle("Ongoing events")
-      .setDescription("Events taken from HLTV")
-      .setURL(url);
+    if (subCommand === "ongoing-events") {
 
-    ongoingEventEmbedList.push(ongoingEventHeaderEmbed);
+      axios.get(url + "/events").then(urlResponse => {
 
-    axios.get(url + "/events").then(urlResponse => {
+        //const event = new GuildScheduledEvent();
+        let ongoingEventEmbedList = [];
 
-      //urlResponse.data is the whole html code of that page
-      const html = urlResponse.data;
-      const $ = cheerio.load(html);
+        let ongoingEventHeaderEmbed = new EmbedBuilder()
+          .setColor(0xFF0000)
+          .setTitle("Ongoing events")
+          .setDescription("Events taken from HLTV")
+          .setURL(url);
 
-      //Ongoing events
-      $("#FEATURED").find(".ongoing-event-holder").find('a').each((i, a) => {
+        channel.send({ embeds: [ongoingEventHeaderEmbed] });
 
-        let ongoingEventTitle = $(a).find("div.content.standard-box").find("img.logo").attr("title");
-        console.log(ongoingEventTitle);
+        //urlResponse.data is the whole html code of that page
+        const html = urlResponse.data;
+        const $ = cheerio.load(html);
 
-        let ongoingEventLink = $(a).attr("href");
-        ongoingEventLink = url + ongoingEventLink;
+        //Ongoing events
+        $("#FEATURED").find(".ongoing-event-holder").find('a').each((i, a) => {
 
-        let ongoingEventImgLink = $(a).find("img.logo").attr("src");
+          let ongoingEventTitle = $(a).find("div.content.standard-box").find("img.logo").attr("title");
+          console.log(ongoingEventTitle);
 
-        let ongoingEventEmbed = new EmbedBuilder()
-          .setColor(0xFFFFFF)
-          .setTitle(ongoingEventTitle)
-          .setThumbnail(ongoingEventImgLink)
-          .setURL(ongoingEventLink);
+          let ongoingEventOrganizer = ongoingEventTitle.split(' ')[0];
 
-        //add all the embeds to the list so they all comes out as one
-        //ongoingEventEmbedList.push(ongoingEventEmbed);
-        channel.send({ embeds: [ongoingEventEmbed], fetchReply: true });
-        console.log("Ongoing Event(s): \t" + ongoingEventLink);
-      })
+          let ongoingEventDate = $(a).find("span.col-desc").find("span").find("span").text();
+          let dateArray = ongoingEventDate.split(' ');
+          let dateArrayLength = dateArray.length;
+          ongoingEventDate = (`${dateArray[0]} ${dateArray[1]} - ${dateArray[dateArrayLength - 3]} ${dateArray[dateArrayLength - 2]}`);
+          ongoingEventDate = ongoingEventDate.slice(0, -3);
+          console.log(ongoingEventDate);
 
-      //===================================================
+          let ongoingEventLink = $(a).attr("href");
+          ongoingEventLink = url + ongoingEventLink;
 
+          let ongoingEventOrganizerImgLink = $(a).find("img.logo").attr("src");
+
+          let ongoingEventEmbed = new EmbedBuilder()
+            .setAuthor({ name: ongoingEventOrganizer, iconURL: ongoingEventOrganizerImgLink })
+            .setColor(0xFFFFFF)
+            .setTitle(ongoingEventTitle)
+            .setDescription("Date: " + ongoingEventDate)
+            .setThumbnail(ongoingEventOrganizerImgLink)
+            .setURL(ongoingEventLink);
+
+          //add all the embeds to the list so they all comes out as one
+          //ongoingEventEmbedList.push(ongoingEventEmbed);
+          channel.send({ embeds: [ongoingEventEmbed], fetchReply: true });
+          console.log("Ongoing Event(s): \t" + ongoingEventLink);
+        });
+      });
+
+      await interaction.reply({ content: "Upcomging Event" });
+
+    } else if (subCommand === "upcoming-events") {
+
+      //header embed for upcoming events
       let upcomingEventHeaderEmbed = new EmbedBuilder()
         .setColor(0x2b6ea3)
         .setTitle("Upcoming events")
@@ -72,69 +101,60 @@ module.exports = {
 
       channel.send({ embeds: [upcomingEventHeaderEmbed] });
 
-      //loop through each month
-      $('div.events-holder').each((i, month) => {
+      axios.get(url + "/events").then(urlResponse => {
 
-        let upcomingEventsMonthGrid = $(month).find("a.big-event");
+        //urlResponse.data is the whole html code of that page
+        const html = urlResponse.data;
+        const $ = cheerio.load(html);
 
-        //loop through all the event in that month
-        $(upcomingEventsMonthGrid).each((i, event) => {
-          let upcomingEventTitle = $(event).find("div.big-event-name").text();
-          console.log("Events: \t" + upcomingEventTitle);
+        //loop through each month
+        $('div.events-holder').each((i, month) => {
 
-          let upcomingEventOrganizer = upcomingEventTitle.split(' ')[0];
+          let upcomingEventsMonthGrid = $(month).find("a.big-event");
 
-          //have to seperate the date because its a mess
-          let upcomingEventDate = $(event).find("td.col-value.col-date").find("span").text();
-          let dateArray = upcomingEventDate.split(' ');
-          let dateArrayLength = dateArray.length;
-          upcomingEventDate = (`${dateArray[0]} ${dateArray[1]} - ${dateArray[dateArrayLength - 3]} ${dateArray[dateArrayLength - 2]}`);
-          upcomingEventDate = upcomingEventDate.slice(0, -3);
-          //console.log(upcomingEventDate);
+          //loop through all the event in that month
+          $(upcomingEventsMonthGrid).each((i, event) => {
+            let upcomingEventTitle = $(event).find("div.big-event-name").text();
+            //console.log("Events: \t" + upcomingEventTitle);
 
-          let upcomingEventLink = url + $(event).attr("href");
+            let upcomingEventOrganizer = upcomingEventTitle.split(' ')[0];
 
-          console.log(upcomingEventLink);
+            //have to seperate the date because its a mess
+            let upcomingEventDate = $(event).find("td.col-value.col-date").find("span").text();
+            let dateArray = upcomingEventDate.split(' ');
+            let dateArrayLength = dateArray.length;
+            upcomingEventDate = (`${dateArray[0]} ${dateArray[1]} - ${dateArray[dateArrayLength - 3]} ${dateArray[dateArrayLength - 2]}`);
+            upcomingEventDate = upcomingEventDate.slice(0, -3);
+            //console.log(upcomingEventDate);
 
-          let upcomingEventBannerUrl = $(event)
-            .find("img.event-header")
-            .attr("src");
+            let upcomingEventLink = url + $(event).attr("href");
 
-          if (!isWebsite(upcomingEventBannerUrl)) {
-            //if there are no image link set to default image
-            upcomingEventBannerUrl = "https://www.hltv.org/img/static/event/1.png";
-          }
+            //console.log(upcomingEventLink);
 
-          let teamList = [];
-          axios.get(upcomingEventLink).then(eventUrl => {
-            const html = eventUrl.data;
-            const $ = cheerio.load(html);
+            let upcomingEventBannerUrl = $(event)
+              .find("img.event-header")
+              .attr("src");
 
-            teamList = [];
-            $("div.teams-attending.grid").find("div.team-box").each((index, team) => {
-              let teamName = $(team).find("img").attr("alt");
-              teamList.push(teamName);
+            if (!isWebsite(upcomingEventBannerUrl)) {
+              //if there are no image link set to default image
+              upcomingEventBannerUrl = "https://www.hltv.org/img/static/event/1.png";
+            }
 
-            });
+            let upcomingEventEmbed = new EmbedBuilder()
+              .setAuthor({ name: upcomingEventOrganizer })
+              .setColor(0xFFFF00)
+              .setTitle(upcomingEventTitle)
+              .setDescription("Date: " + upcomingEventDate)
+              .setURL(upcomingEventLink)
+              .setImage(upcomingEventBannerUrl);
+
+            channel.send({ embeds: [upcomingEventEmbed] });
           });
-
-
-          console.log(teamList.length);
-
-          let upcomingEventEmbed = new EmbedBuilder()
-            .setAuthor({ name: upcomingEventOrganizer })
-            .setColor(0xFFFF00)
-            .setTitle(upcomingEventTitle)
-            .setDescription("Date: " + upcomingEventDate)
-            .setURL(upcomingEventLink)
-            .setImage(upcomingEventBannerUrl);
-
-          channel.send({ embeds: [upcomingEventEmbed] });
         });
       });
-    });
 
-    console.log(JSON.stringify(ongoingEventEmbedList));
-    const eventReply = await interaction.reply({ content: `Events`, embeds: ongoingEventEmbedList, ephemeral: false, fetchReply: true });
+      await interaction.reply({ content: "Ongoing Event" });
+
+    }
   },
 };
