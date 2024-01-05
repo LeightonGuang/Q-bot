ae4d44efd77d6609e05159a575bbc3838940ea45
LeightonGuang <leighton.guang@icloud.com>
2024-01-05 17:40:35 +0000

    fix: reply when command failed

--- src/utils/valorant/registered.js
+++ src/utils/valorant/registered.js
@@ -1,19 +1,30 @@
 const fs = require("fs");
 
-module.exports = (userId) => {
+module.exports = (interaction, userId, isOther) => {
   let dataFile = fs.readFileSync("data.json");
   let dataObj = JSON.parse(dataFile);
 
-  const playerRegistered = dataObj.playerList.find((obj) => obj.id === userId);
+  const isPlayerRegistered = dataObj.playerList.find(
+    (obj) => obj.id === userId
+  );
 
-  if (playerRegistered) {
+  if (isPlayerRegistered) {
     return true;
-  } else if (!playerRegistered) {
-    interaction.reply({
-      content:
-        "Please use the command ***/account add-riot-account*** to add a riot account",
-      ephemeral: true,
-    });
-    return false;
+  } else if (!isPlayerRegistered) {
+    if (isOther) {
+      interaction.reply({
+        content:
+          "The account you are looking for does not exist. Please search for a registered account.",
+        ephemeral: true,
+      });
+      return false;
+    } else if (!isOther) {
+      interaction.reply({
+        content:
+          "Please use the command ***/account add-riot-account*** to add a riot account",
+        ephemeral: true,
+      });
+      return false;
+    }
   }
 };
