0bbd986e9ff92bcf7a17aca292fbfc31fe4d1d70
LeightonGuang <leighton.guang@icloud.com>
2024-01-06 20:16:38 +0000

    feat: map win percentage working

--- src/sub-commands/valorant/map-win-percentage.js
+++ src/sub-commands/valorant/map-win-percentage.js
@@ -1,10 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
-const fs = require("node:fs");
+const fs = require("fs");
 const puppeteer = require("puppeteer-extra");
 
 module.exports = async (interaction) => {
-  let dataFile = fs.readFileSync("data.json");
-  let dataObj = JSON.parse(dataFile);
+  const dataFile = fs.readFileSync("data.json");
+  const dataObj = JSON.parse(dataFile);
 
   let userId = interaction.options.getMember("player");
 
@@ -16,16 +16,29 @@ module.exports = async (interaction) => {
   }
 
   const registered = require("../../utils/valorant/registered");
-  let profileUrl = require("../../utils/valorant/profileUrl");
+  const profileUrl = require("../../utils/valorant/profileUrl");
 
-  let userObj = dataObj.playerList.find((obj) => obj.id === userId);
-  let accountObj = userObj.riotAccountList.find((obj) => obj.active === true);
-  let riotId = accountObj.riotId;
-  let trackerProfileMapsUrl = profileUrl(riotId) + "/maps";
+  if (!registered(interaction, userId)) return;
 
-  if (!registered(userId)) return;
+  const userObj = dataObj.playerList.find((obj) => obj.id === userId);
+  const accountObj = userObj.riotAccountList.find((obj) => obj.active === true);
+  const riotId = accountObj.riotId;
+  const trackerProfileMapsUrl = profileUrl(riotId) + "/maps";
 
-  interaction.reply({ content: "Loading info..." });
+  const embedList = [];
+
+  const embedHeader = new EmbedBuilder()
+    .setTitle(riotId)
+    .setURL(trackerProfileMapsUrl)
+    .setDescription("Map Win Percentage");
+
+  embedList.push(embedHeader);
+
+  await interaction.reply({
+    embeds: embedList,
+    content: "Loading info...",
+    fetchReply: true,
+  });
 
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -33,30 +46,75 @@ module.exports = async (interaction) => {
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto(trackerProfileMapsUrl);
 
-  const mapWin = await page.evaluate(() => {
-    const allMaps = Array.from(document.querySelectorAll(".st-content__item"));
+  const mapStats = await page.evaluate(() => {
+    const allMaps = document.querySelectorAll(".st-content__item");
+    const allBackgroundImg = document.querySelectorAll("--background-image");
 
-    const allMapsList = Array.from(
-      document.querySelectorAll(".st-content__item")
-    )
-      .flatMap((stat) => Array.from(stat.querySelectorAll(".info .value")))
-      .map((element) => element.textContent);
+    const mapStatsList = [];
 
-    const mapStats = allMaps.map((map) => ({
-      mapName: allMapsList[map * 15],
-      winPct: allMapsList[map * 15 + 1],
-      /**win: map,
-      loss: map,
-      KD: map,
-      attackWinPct: map,
-      defenseWinPct: map,
-      **/
-    }));
+    allMaps.forEach((map) => {
+      const allStats = map.querySelectorAll(".info .value");
 
-    return mapStats;
+      const mapStats = {
+        mapName: allStats[0].textContent,
+        winPercentage: allStats[1].textContent,
+        wins: allStats[2].textContent,
+        losses: allStats[3].textContent,
+        kd: allStats[4].textContent,
+        adr: allStats[5].textContent,
+        acs: allStats[6].textContent,
+      };
+
+      mapStatsList.push(mapStats);
+    });
+
+    return mapStatsList;
   });
 
-  console.log(mapWin);
+  mapStats.forEach((map) => {
+    const mapEmbed = new EmbedBuilder()
+      .setAuthor({
+        name: map.mapName,
+      })
+      .addFields(
+        {
+          name: "Win %",
+          value: map.winPercentage,
+          inline: true,
+        },
+        {
+          name: "Wins",
+          value: map.wins,
+          inline: true,
+        },
+        {
+          name: "Losses",
+          value: map.losses,
+          inline: true,
+        },
+        {
+          name: "K/D",
+          value: map.kd,
+          inline: true,
+        },
+        {
+          name: "ADR",
+          value: map.adr,
+          inline: true,
+        },
+        {
+          name: "ACS",
+          value: map.acs,
+          inline: true,
+        }
+      );
+
+    embedList.push(mapEmbed);
+    interaction.editReply({
+      content: "",
+      embeds: embedList,
+    });
+  });
 
   await page.screenshot({ path: "screenshot.png", fullPage: true });
   console.log("LOG: \t" + "screenshot");
