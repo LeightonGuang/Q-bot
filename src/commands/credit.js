3fd2ab0fc9029a13437eb2611aea55226f8c9042
LeightonGuang <leighton.guang@icloud.com>
2024-01-22 12:39:35 +0000

    style: rearrange code

--- src/commands/credit.js
+++ src/commands/credit.js
@@ -1,23 +1,21 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, Embed } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("credit")
-    .setDescription("Thank you to all the people that helped")
-  ,
-
+    .setDescription("Thank you to all the people that helped"),
   async execute(interaction) {
     let creditEmbed = new EmbedBuilder()
       .setColor(0xffd700)
       .setAuthor({ name: "Q bot" })
       .setTitle("Credit")
       .setDescription("Thank you to the people that helped made Q bot")
-      .addFields(
-        { name: "<:Pog:1139840279659159582>", value: "<@691784137790717984>" }
-      )
-      .setTimestamp()
-
+      .addFields({
+        name: "<:Pog:1139840279659159582>",
+        value: "<@691784137790717984>",
+      })
+      .setTimestamp();
 
     interaction.reply({ embeds: [creditEmbed] });
-  }
-}
+  },
+};
