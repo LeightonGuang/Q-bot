e02968fb28ade15140738a35c41b50bfddde44cc
Leighton Guang <leighton.guang@icloud.com>
2024-02-15 04:18:56 +0000

    refactor: move edit-riot-account to another file

--- src/commands/account.js
+++ src/commands/account.js
@@ -233,54 +233,8 @@ module.exports = {
         break;
       }
       case "edit-riot-account": {
-        let rank = interaction.options.get("rank")?.value;
-        let riotId = interaction.options.get("riot-id")?.value;
-        let region = interaction.options.get("region")?.value;
-
-        if (rank || riotId || region) {
-          let memberId = interaction.member.id;
-          let playerObj = playerList.find((obj) => obj.id === memberId);
-          let riotAccount = playerObj.riotAccountList.find(
-            (obj) => obj.active === true
-          );
-
-          if (rank) {
-            riotAccount.rank = rank;
-            writeToFile(dataObj, "data.json");
-            console.log("LOG:\t" + `member has edited their rank to ${rank}`);
-          }
-
-          if (riotId) {
-            riotAccount.riotId = riotId;
-            writeToFile(dataObj, "data.json");
-            console.log(
-              "LOG:\t" + `member has edited their Riot id to ${riotId}`
-            );
-          }
-
-          if (region) {
-            riotAccount.region = region;
-            writeToFile(dataObj, "data.json");
-            console.log(
-              "LOG:\t" + `member has edited their region to ${region}`
-            );
-          }
-
-          let riotAccountEmbed = new EmbedBuilder()
-            .setColor(0x3ba55b)
-            .setTitle(riotAccount.riotId)
-            .addFields(
-              { name: "Rank:", value: riotAccount.rank },
-              { name: "Region:", value: riotAccount.region }
-            );
-
-          await interaction.reply({
-            content: "Riot account edited successfully",
-            embeds: [riotAccountEmbed],
-            ephemeral: true,
-          });
-        }
-
+        const editRiotAccount = require("../sub-commands/account/edit-riot-account");
+        editRiotAccount(interaction);
         break;
       }
       case "edit-steam-account": {
