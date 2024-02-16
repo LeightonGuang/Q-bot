41f7100c6554539fc3eba3244fd621bbdd11b436
Leighton Guang <leighton.guang@icloud.com>
2024-02-16 05:04:57 +0000

    feat: change .env import

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,52 +1,59 @@
 //ONLY DEPLOY WHEN COMMAND IS WORKING OR DONE
 
-const { REST, Routes } = require('discord.js');
-const { config } = require('dotenv');
-const fs = require('node:fs');
+import { REST, Routes } from "discord.js";
+import { config } from "dotenv";
+import fs from "node:fs";
+import dotenv from "dotenv";
+dotenv.config({ path: "../public/.env" });
 
 config();
 
 const TOKEN = process.env.TOKEN;
-const GUILD_ID_LIST = process.env.GUILD_ID_LIST.split(',');
+const GUILD_ID_LIST = process.env.GUILD_ID_LIST.split(",");
 const CLIENT_ID = process.env.CLIENT_ID;
 console.log("LOG: \t .env loaded");
 console.log(`
 TOKEN: ${TOKEN}
 GUILD_ID_LIST: ${GUILD_ID_LIST}
 CLIENT_ID: ${CLIENT_ID}
-`)
+`);
 
 for (let GUILD_ID of GUILD_ID_LIST) {
   const commands = [];
   // Grab all the command files from the commands directory you created earlier
-  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+  const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
 
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = await import(`./commands/${file}`);
     console.log("| ✅ | " + file);
     commands.push(command.data.toJSON());
   }
 
   // Construct and prepare an instance of the REST module
-  const rest = new REST({ version: '10' }).setToken(TOKEN);
+  const rest = new REST({ version: "10" }).setToken(TOKEN);
 
   // and deploy your commands!
   (async () => {
     try {
-      console.log(`Started refreshing ${commands.length} application (/) commands.`);
+      console.log(
+        `Started refreshing ${commands.length} application (/) commands.`
+      );
 
       // The put method is used to fully refresh all commands in the guild with the current set
       const data = await rest.put(
         Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-        { body: commands },
+        { body: commands }
       );
 
-      console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+      console.log(
+        `Successfully reloaded ${typeof data} application (/) commands.`
+      );
     } catch (error) {
       // And of course, make sure you catch and log any errors!
       console.error(error);
     }
   })();
-
 }
