134ac4279f1a4149d83d530549458f1cc76b0bbb
Leighton Guang <leighton.guang@icloud.com>
2024-02-18 05:35:07 +0000

    feat: remove /overview from url

--- src/utils/profileUrl.ts
+++ src/utils/profileUrl.ts
@@ -2,5 +2,5 @@ export function profileUrl(riotId: string) {
   let modifiedId = riotId.replace(/\s+/g, "%20");
   modifiedId = modifiedId.replace(/#/g, "%23");
 
-  return "https://tracker.gg/valorant/profile/riot/" + modifiedId + "/overview";
+  return "https://tracker.gg/valorant/profile/riot/" + modifiedId;
 }
