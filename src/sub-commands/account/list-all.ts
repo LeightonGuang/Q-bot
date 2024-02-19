3f239792468cc9d47e8ed6d6d2a3adbf0a5753e5
Leighton Guang <leighton.guang@icloud.com>
2024-02-19 06:31:27 +0000

    feat: handle no player object, no riot account and no steam account

--- src/sub-commands/account/list-all.ts
+++ src/sub-commands/account/list-all.ts
@@ -16,6 +16,18 @@ export const subCommand = async (interaction) => {
   let playerId = interaction.member.id;
   let playerObj = playerList.find((obj) => obj.id === playerId);
 
+  if (!playerObj) {
+    const alertEmbed = new EmbedBuilder()
+      .setColor(0xff4553)
+      .setTitle("Error")
+      .setDescription(
+        "You don't have any accounts linked to your discord account"
+      );
+
+    await interaction.reply({ embeds: [alertEmbed], ephemeral: true });
+    return;
+  }
+
   let accountEmbedList = [];
 
   //embed indicating steam accounts
@@ -25,27 +37,34 @@ export const subCommand = async (interaction) => {
 
   accountEmbedList.push(riotHeaderEmbed);
 
-  for (let riotAccountObj of playerObj.riotAccountList) {
-    let riotAccountEmbed = new EmbedBuilder()
-      .setTitle(riotAccountObj.riotId)
-      .addFields([
-        { name: "Region:", value: riotAccountObj.region, inline: true },
-        { name: "Rank:", value: riotAccountObj.rank, inline: true },
-        {
-          name: "Active:",
-          value: riotAccountObj.active.toString(),
-          inline: true,
-        },
-      ]);
+  if (playerObj.riotAccountList.length === 0) {
+    let noRiotAccountEmbed = new EmbedBuilder()
+      .setColor(0xff4553)
+      .setTitle("No riot accounts found");
+    accountEmbedList.push(noRiotAccountEmbed);
+  } else if (playerObj.riotAccountList.length > 0) {
+    for (let riotAccountObj of playerObj.riotAccountList) {
+      let riotAccountEmbed = new EmbedBuilder()
+        .setTitle(riotAccountObj.riotId)
+        .addFields([
+          { name: "Region:", value: riotAccountObj.region, inline: true },
+          { name: "Rank:", value: riotAccountObj.rank, inline: true },
+          {
+            name: "Active:",
+            value: riotAccountObj.active.toString(),
+            inline: true,
+          },
+        ]);
 
-    if (riotAccountObj.active === true) {
-      riotAccountEmbed.setColor(0x3ba55b);
-    } else if (riotAccountObj.active === false) {
-      riotAccountEmbed.setColor(0xec4245);
+      if (riotAccountObj.active === true) {
+        riotAccountEmbed.setColor(0x3ba55b);
+      } else if (riotAccountObj.active === false) {
+        riotAccountEmbed.setColor(0xec4245);
+      }
+
+      //console.log(JSON.stringify(riotAccountEmbed));
+      accountEmbedList.push(riotAccountEmbed);
     }
-
-    //console.log(JSON.stringify(riotAccountEmbed));
-    accountEmbedList.push(riotAccountEmbed);
   }
 
   //embed indicating steam accounts
@@ -55,21 +74,28 @@ export const subCommand = async (interaction) => {
 
   accountEmbedList.push(steamHeaderEmbed);
 
-  for (let steamAccountObj of playerObj.steamAccountList) {
-    let steamAccountEmbed = new EmbedBuilder()
-      .setTitle(steamAccountObj.accountName)
-      .setFields({
-        name: "LINK:",
-        value: `[profile](${steamAccountObj.steamProfileUrl})`,
-      });
+  if (playerObj.steamAccountList.length === 0) {
+    let noSteamAccountEmbed = new EmbedBuilder()
+      .setColor(0x2a475e)
+      .setTitle("No steam accounts found");
+    accountEmbedList.push(noSteamAccountEmbed);
+  } else if (playerObj.steamAccountList.length > 0) {
+    for (let steamAccountObj of playerObj.steamAccountList) {
+      let steamAccountEmbed = new EmbedBuilder()
+        .setTitle(steamAccountObj.accountName)
+        .setFields({
+          name: "LINK:",
+          value: `[profile](${steamAccountObj.steamProfileUrl})`,
+        });
 
-    if (steamAccountObj.active === true) {
-      steamAccountEmbed.setColor(0x3ba55b);
-    } else if (steamAccountObj.active === false) {
-      steamAccountEmbed.setColor(0x2a475e);
+      if (steamAccountObj.active === true) {
+        steamAccountEmbed.setColor(0x3ba55b);
+      } else if (steamAccountObj.active === false) {
+        steamAccountEmbed.setColor(0x2a475e);
+      }
+
+      accountEmbedList.push(steamAccountEmbed);
     }
-
-    accountEmbedList.push(steamAccountEmbed);
   }
 
   await interaction.reply({
