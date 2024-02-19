7ad55f15236024eecfb9f9e746d1e80fb4aa9a42
Leighton Guang <leighton.guang@icloud.com>
2024-02-19 05:43:16 +0000

    feat: create typescript version

---
+++ src/sub-commands/valorant/ongoing-events.ts
@@ -0,0 +1,26 @@
+import { EmbedBuilder } from "discord.js";
+import { fetchEvents } from "../../utils/valorant/fetchEvents.js";
+import { sendEmbed } from "../../utils/valorant/sendEmbed.js";
+
+export const subCommand = async (interaction) => {
+  const year: number = new Date().getFullYear();
+  const vlrUrl: string = "https://vlr.gg";
+
+  let valorantEventEmbedList: EmbedBuilder[] = [];
+
+  let ongoingEventEmbedHeader = new EmbedBuilder()
+    .setColor(0xffffff)
+    .setTitle("Ongoing Valorant Champions Tour " + year)
+    .setURL(vlrUrl + "/vct-" + year)
+    .setDescription("Riot's official " + year + " Valorant tournament circuit");
+
+  valorantEventEmbedList.push(ongoingEventEmbedHeader);
+
+  await interaction.reply({
+    embeds: valorantEventEmbedList,
+    fetchReply: true,
+  });
+
+  const ongoingEventEmbedList: object[] = await fetchEvents(interaction);
+  await sendEmbed(interaction, ongoingEventEmbedList);
+};
