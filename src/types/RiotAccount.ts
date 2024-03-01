ef16cb5c246368b942e21670bad1de64eec90aae
Leighton Guang <leighton.guang@icloud.com>
2024-03-01 15:39:15 +0000

    feat: create types in seperate files

---
+++ src/types/RiotAccount.ts
@@ -0,0 +1,8 @@
+export type RiotAccount = {
+  id: number;
+  discord_id: string;
+  riot_id: string;
+  region: string;
+  rank: string;
+  active: boolean;
+};
---
+++ src/types/SteamAccount.ts
@@ -0,0 +1,9 @@
+export type SteamAccount = {
+  id: number;
+  discord_id: string;
+  steam_id: string;
+  account_name: string;
+  friend_code: number;
+  steam_profile_url: string;
+  active: boolean;
+};
