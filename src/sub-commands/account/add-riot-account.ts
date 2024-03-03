d7f37591e21edc2d13865b6d2fdac2cef69ff4a6
Leighton Guang <leighton.guang@icloud.com>
2024-03-03 21:06:56 +0000

    feat: update command to use api to add riot account

--- src/sub-commands/account/add-riot-account.ts
+++ src/sub-commands/account/add-riot-account.ts
@@ -1,52 +1,18 @@
-import fs from "fs";
-import { writeToFile } from "../../utils/writeToFile.js";
-import { fileURLToPath } from "url";
-import path from "path";
+import axios from "axios";
+import { RiotAccount } from "../../types/RiotAccount.js";
 
 export const subCommand = async (interaction) => {
+  const discordId: number = interaction.member.id;
+  const userTag: string = interaction.member.user.tag;
   const riotId: string = interaction.options.get("riot-id").value;
-  const playerId: number = interaction.member.id;
-  const playerTag: string = interaction.member.user.tag;
+  const region: string = interaction.options.get("region").value;
+  const rank: string = interaction.options.get("rank").value;
 
-  const currentFilePath: string = fileURLToPath(import.meta.url);
-  const dataFilePath: string = path.resolve(
-    path.dirname(currentFilePath),
-    "../../../public/data.json"
-  );
-  const dataFile: string = fs.readFileSync(dataFilePath, "utf-8");
-  type RiotAccountObj = {
-    riotId: string;
-    region: string;
-    rank: string;
-    active: boolean;
-  };
-
-  type SteamAccountObj = {
-    accountName: string;
-    friendCode: number;
-    steamProfileUrl: string;
-    active: boolean;
-  };
-
-  type PlayerObj = {
-    id: number;
-    tag: string;
-    riotAccountList: RiotAccountObj[];
-    steamAccountList: SteamAccountObj[];
-  };
-
-  type DataObject = {
-    playerList: PlayerObj[];
-  };
-
-  const dataObj: DataObject = JSON.parse(dataFile);
-  const playerList: PlayerObj[] = dataObj.playerList;
-  let playerObj: PlayerObj = playerList.find((obj) => obj.id === playerId);
   const [name, tag]: string[] = riotId.split("#");
 
   if (!tag) {
     interaction.reply({
-      content: "Please include a tag in the riot Id",
+      content: "Please include a tag in your riot Id",
       ephemeral: true,
     });
     return;
@@ -55,75 +21,86 @@ export const subCommand = async (interaction) => {
   if (name.length < 3 || name.length > 16 || tag.length < 3 || tag.length > 5) {
     interaction.reply({
       content:
-        "Keep your riot id between 3-16 characters\n#tag between 3-5 characters",
+        "Keep the riot id between 3-16 characters\n#tag between 3-5 characters",
       ephemeral: true,
     });
     return;
   }
 
-  // create player object if not exist
-  if (!playerObj) {
-    playerObj = {
-      id: playerId,
-      tag: playerTag,
-      riotAccountList: [],
-      steamAccountList: [],
-    };
+  try {
+    console.log("check if user exist");
+    const { data: userExist } = await axios.get(
+      "http://localhost:8080/api/accounts/user_exist/" + discordId
+    );
 
-    playerList.push(playerObj);
-    writeToFile(dataObj);
-  }
-
-  const region: string = interaction.options.get("region").value;
-  const rank: string = interaction.options.get("rank").value;
-
-  const riotAccountList: RiotAccountObj[] = playerObj.riotAccountList;
-  const riotIdDuplicate: RiotAccountObj = riotAccountList.find(
-    (obj) => obj.riotId === riotId
-  );
-
-  if (riotIdDuplicate) {
-    // check for duplicate riot id
-    await interaction.reply({
-      content: "You've already added this account.",
-      ephemeral: true,
-    });
-    console.log("LOG: \t" + "riot id already added");
-    return;
-  }
-
-  if (riotAccountList) {
-    // make all old riot accounts inactive
-    for (let riotAccount of riotAccountList) {
-      riotAccount.active = false;
+    if (!userExist) {
+      console.log("if user not exist");
+      try {
+        // create new account
+        await axios.post("http://localhost:8080/api/accounts", {
+          discord_id: discordId,
+          tag: userTag,
+        });
+        console.log("LOG: \t" + "new account created");
+      } catch (error) {
+        console.error(error);
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
 
-  let riotAccountObj = {
-    riotId: riotId,
-    region: region,
-    rank: rank,
-    active: true,
-  };
+  // check for duplicate riot id
+  try {
+    console.log("check for duplicate riot id");
+    const { data: isDuplicateRiotId }: { data: boolean } = await axios.get(
+      "http://localhost:8080/api/accounts/riot/is_duplicate",
+      {
+        data: { discord_id: discordId, riot_id: riotId },
+      }
+    );
 
-  playerObj.riotAccountList.push(riotAccountObj);
-  writeToFile(dataObj);
+    if (isDuplicateRiotId) {
+      await interaction.reply({
+        content: "You've already added this account.",
+        ephemeral: true,
+      });
+      console.log("LOG: \t" + "riot id already added");
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 
-  await interaction.reply({
-    content:
+  // add new riot account
+  try {
+    console.log("add new riot account");
+    await axios.post("http://localhost:8080/api/accounts/riot/add", {
+      discord_id: discordId,
+      riot_id: riotId,
+      region: region,
+      rank: rank,
+      active: true,
+    });
+
+    await interaction.reply({
+      content:
+        "new riot account added\n" +
+        `tag:\t${userTag}\n` +
+        `riot-id:\t${riotId}\n` +
+        `region:\t${region}\n` +
+        `rank:\t${rank}`,
+      ephemeral: true,
+    });
+
+    console.log(
       "new riot account added\n" +
-      `tag:\t${playerTag}\n` +
-      `riot-id:\t${riotId}\n` +
-      `region:\t${region}\n` +
-      `rank:\t${rank}`,
-    ephemeral: true,
-  });
-
-  console.log(
-    "new riot account added\n" +
-      `tag:\t${playerTag}\n` +
-      `riot-id:\t${riotId}\n` +
-      `region:\t${region}\n` +
-      `rank:\t${rank}`
-  );
+        `tag:\t${userTag}\n` +
+        `riot-id:\t${riotId}\n` +
+        `region:\t${region}\n` +
+        `rank:\t${rank}`
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
