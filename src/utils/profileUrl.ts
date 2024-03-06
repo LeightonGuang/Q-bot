39ec5c25d1936bb4c0d8d0d5361cbb10c6c407a2
Leighton Guang <leighton.guang@icloud.com>
2024-03-06 13:46:00 +0000

    refactor: move files to valorant directory

--- src/utils/profileUrl.ts
+++
@@ -1,6 +0,0 @@
-export function profileUrl(riotId: string) {
-  let modifiedId = riotId.replace(/\s+/g, "%20");
-  modifiedId = modifiedId.replace(/#/g, "%23");
-
-  return "https://tracker.gg/valorant/profile/riot/" + modifiedId;
-}
