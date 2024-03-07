11e92cb487544a3b7e60c040840cdcaf3eeca1db
Leighton Guang <leighton.guang@icloud.com>
2024-03-07 11:55:22 +0000

    feat: handle if player has an account but no riot account

--- src/utils/valorant/registered.ts
+++ src/utils/valorant/registered.ts
@@ -1,17 +1,39 @@
 import axios from "axios";
+import { RiotAccount } from "../../types/RiotAccount.js";
 
 export async function registered(interaction: any, discordId: string) {
-  const playerRegistered: boolean = await axios.get(
-    "http://localhost:8080/api/accounts/registered/" + discordId
-  );
-  if (playerRegistered) {
-    return true;
-  } else if (!playerRegistered) {
-    interaction.reply({
-      content:
-        "Please use the command ***/account add-riot-account*** to add a riot account",
-      ephemeral: true,
-    });
-    return false;
+  try {
+    const playerRegistered: boolean = await axios.get(
+      "http://localhost:8080/api/accounts/registered/" + discordId
+    );
+    if (playerRegistered) {
+      try {
+        const riotAccountList: RiotAccount[] = await axios.get(
+          "http://localhost:8080/api/accounts/riot/get/" + discordId
+        );
+
+        if (riotAccountList.length === 0) {
+          interaction.reply({
+            content:
+              "Please use the command ***/account add-riot-account*** to add a riot account",
+            ephemeral: true,
+          });
+          return false;
+        } else if (riotAccountList.length > 0) {
+          return true;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (!playerRegistered) {
+      interaction.reply({
+        content:
+          "Please use the command ***/account add-riot-account*** to add a riot account",
+        ephemeral: true,
+      });
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
