a0e5346581b1efc36dddeafc6b71a63bb8662854
Leighton Guang <leighton.guang@icloud.com>
2024-03-12 22:34:13 +0000

    delete: javascript code files

--- src/utils/valorant/profileUrl.js
+++
@@ -1,8 +0,0 @@
-module.exports = (riotId) => {
-  //gets the tracker.gg profile url using their riot id
-  let modifiedId = riotId.replace(/\s+/g, "%20");
-  modifiedId = modifiedId.replace(/#/g, "%23");
-
-  let playerUrl = "https://tracker.gg/valorant/profile/riot/" + modifiedId;
-  return playerUrl;
-};
