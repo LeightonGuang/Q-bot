d6d92070749db728baecaac7aacf8ec639ef7199
Leighton Guang <leighton.guang@icloud.com>
2024-03-12 22:52:46 +0000

    feat: export profileUrl as default

--- src/utils/valorant/profileUrl.ts
+++ src/utils/valorant/profileUrl.ts
@@ -1,6 +1,8 @@
-export function profileUrl(riotId: string) {
+const profileUrl = (riotId: string) => {
   let modifiedId = riotId.replace(/\s+/g, "%20");
   modifiedId = modifiedId.replace(/#/g, "%23");
 
   return "https://tracker.gg/valorant/profile/riot/" + modifiedId;
-}
+};
+
+export default profileUrl;
