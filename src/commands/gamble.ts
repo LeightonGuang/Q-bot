155b6a5e0b337e383e6cef8b105ec821eb83a117
Leighton Guang <leighton.guang@icloud.com>
2024-03-14 16:50:26 +0000

    feat: check if user has enough qoins to gamble

--- src/commands/gamble.ts
+++ src/commands/gamble.ts
@@ -1,4 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
+import axios from "axios";
+import { Balance } from "../types/Balance.js";
 
 export const data: any = {
   data: new SlashCommandBuilder()
@@ -17,6 +19,30 @@ export const data: any = {
     ),
 
   async execute(interaction) {
+    const bet: number = interaction.options.get("qoins").value;
+
+    // check if user has enough qoins
+    try {
+      const { data }: { data: Balance[] } = await axios.get(
+        "http://localhost:8080/api/account/balance/get/" + interaction.member.id
+      );
+      if (data[0].balance < bet) {
+        await interaction.reply({
+          content: `You placed a ${bet} Qoin bet, but you only have ${data[0].balance} Qoins`,
+          ephemeral: true,
+        });
+        return;
+      } else if (data[0].balance === 0) {
+        await interaction.reply({
+          content: "You don't have 0 Qoins in your account",
+          ephemeral: true,
+        });
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
     const slot = await import("../sub-commands/gamble/slots.js");
     slot.subCommand(interaction);
   },
