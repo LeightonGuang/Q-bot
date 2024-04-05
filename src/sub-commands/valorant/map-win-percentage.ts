69f4c7a846390e8a8e4908cd8c3ccb2b52224c48
Leighton Guang <leighton.guang@icloud.com>
2024-04-05 09:20:33 +0100

    feat: handle unplayed map error

--- src/sub-commands/valorant/map-win-percentage.ts
+++ src/sub-commands/valorant/map-win-percentage.ts
@@ -5,7 +5,7 @@ import { registered } from "../../utils/valorant/registered.js";
 import profileUrl from "../../utils/valorant/profileUrl.js";
 import { RiotAccount } from "../../types/RiotAccount.js";
 
-export const subCommand = async (interaction) => {
+export const subCommand: any = async (interaction) => {
   const chosenMap: string = interaction.options.getString("map");
   let selectedDiscordId: string = interaction.options.getMember("player");
 
@@ -28,11 +28,12 @@ export const subCommand = async (interaction) => {
     console.error(error);
   }
 
-  const trackerProfileMapsUrl = profileUrl(activeRiotAccount.riot_id) + "/maps";
+  const trackerProfileMapsUrl: string =
+    profileUrl(activeRiotAccount.riot_id) + "/maps";
 
-  const embedList = [];
+  const embedList: EmbedBuilder[] = [];
 
-  const embedHeader = new EmbedBuilder()
+  const embedHeader: EmbedBuilder = new EmbedBuilder()
     .setTitle(activeRiotAccount.riot_id)
     .setURL(trackerProfileMapsUrl)
     .setDescription("Map Win %");
@@ -45,34 +46,37 @@ export const subCommand = async (interaction) => {
     fetchReply: true,
   });
 
-  const browser = await (puppeteer as any).launch({ headless: true });
-  const page = await browser.newPage();
+  const browser: any = await (puppeteer as any).launch({ headless: true });
+  const page: any = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20100101 Firefox/6.0"
+  );
   await page.goto(trackerProfileMapsUrl);
 
   try {
-    const mapStats = await page.evaluate(() => {
-      const allMaps = document.querySelectorAll(".st-content__item");
-      const allBackgroundImg = document.querySelectorAll(
+    const mapStats: any = await page.evaluate(() => {
+      const allMaps: any = document.querySelectorAll(".st-content__item");
+      const allBackgroundImg: any = document.querySelectorAll(
         ".st-content__item .st__item.st-content__item-value.st__item--sticky"
       );
 
-      const allBackgroundImgArray = [];
+      const allBackgroundImgArray: string[] = [];
 
       for (let i = 0; i < allBackgroundImg.length; i++) {
-        const style = allBackgroundImg[i].getAttribute("style");
-        const regex = /url\(['"](.+?)['"]\)/;
+        const style: string = allBackgroundImg[i].getAttribute("style");
+        const regex: RegExp = /url\(['"](.+?)['"]\)/;
 
-        const imgUrl = style.match(regex)[1];
+        const imgUrl: string = style.match(regex)[1];
         allBackgroundImgArray.push(imgUrl);
       }
 
-      const mapStatsList = [];
+      const mapStatsList: any[] = [];
 
       allMaps.forEach((map, index) => {
-        const allStats = map.querySelectorAll(".info .value");
+        const allStats: any = map.querySelectorAll(".info .value");
 
-        const mapStats = {
+        const mapStats: any = {
           mapName: allStats[0].textContent,
           winPercentage: allStats[1].textContent,
           wins: allStats[2].textContent,
@@ -91,7 +95,7 @@ export const subCommand = async (interaction) => {
 
     if (!chosenMap) {
       mapStats.forEach((map) => {
-        const mapEmbed = new EmbedBuilder()
+        const mapEmbed: EmbedBuilder = new EmbedBuilder()
           .setAuthor({
             name: map.mapName,
           })
@@ -136,10 +140,21 @@ export const subCommand = async (interaction) => {
         });
       });
     } else if (chosenMap) {
-      const map = mapStats.find(
+      const map: any = mapStats.find(
         (map) => map.mapName.toLowerCase() === chosenMap
       );
-      const mapEmbed = new EmbedBuilder()
+
+      if (!map) {
+        const errorEmbed: EmbedBuilder = new EmbedBuilder()
+          .setColor(0xff0000)
+          .setTitle(`You have not played a game in ${chosenMap} yet`);
+
+        return interaction.editReply({
+          content: "",
+          embeds: [errorEmbed],
+        });
+      }
+      const mapEmbed: EmbedBuilder = new EmbedBuilder()
         .setAuthor({
           name: map.mapName,
         })
