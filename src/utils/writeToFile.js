62af4f49609faa1abf124985c80b6a3e7c1b931e
Leighton Guang <leighton.guang@icloud.com>
2024-04-14 17:31:48 +0100

    delete: javascript version

--- src/utils/writeToFile.js
+++
@@ -1,7 +0,0 @@
-const fs = require('node:fs');
-
-module.exports = (data, file) => {
-  //data = an object
-  let dataString = JSON.stringify(data, null, 2);
-  fs.writeFileSync(file, dataString);
-}
