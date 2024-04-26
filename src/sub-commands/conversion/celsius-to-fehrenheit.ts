3c18fe769eae81e704b41ad251cc1ab8b688ad9c
Leighton Guang <leighton.guang@icloud.com>
2024-04-26 12:38:53 +0100

    fix: converted result to 1 decimal place

--- src/sub-commands/conversion/celsius-to-fehrenheit.ts
+++ src/sub-commands/conversion/celsius-to-fehrenheit.ts
@@ -2,7 +2,9 @@ import { EmbedBuilder } from "discord.js";
 
 export const subCommand = async (interaction) => {
   const celsius: number = interaction.options.getNumber("celsius");
-  const fahrenheit: number = (celsius * 9) / 5 + 32;
+  let fahrenheit: number = (celsius * 9) / 5 + 32;
+
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
 
   const convertedEmbed: EmbedBuilder = new EmbedBuilder()
     .setColor(0xffffff)
