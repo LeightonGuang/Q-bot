cf4a9482b9311d3fa47542887e11ba841525ad6d
Leighton Guang <leighton.guang@icloud.com>
2024-04-26 12:14:24 +0100

    feat: converter command

---
+++ src/commands/converter.ts
@@ -0,0 +1,65 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export const data = {
+  data: new SlashCommandBuilder()
+    .setName("converter")
+    .setDescription("Commands for converting metric and imperial units")
+    .addSubcommand((addSubcommand) =>
+      addSubcommand
+        .setName("celsius-to-fehrenheit")
+        .setDescription("convert celsius to fahrenheit")
+        .addNumberOption((option) =>
+          option.setName("celsius").setDescription("celsius").setRequired(true)
+        )
+    )
+    .addSubcommand((addSubcommand) =>
+      addSubcommand
+        .setName("fehrenheit-to-celsius")
+        .setDescription("convert fahrenheit to celsius")
+        .addNumberOption((option) =>
+          option
+            .setName("fehrenheit")
+            .setDescription("fehrenheit")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((addSubcommand) =>
+      addSubcommand
+        .setName("kg-to-lbs")
+        .setDescription("convert kg to lbs")
+        .addNumberOption((option) =>
+          option.setName("kg").setDescription("kg").setRequired(true)
+        )
+    )
+    .addSubcommand((addSubcommand) =>
+      addSubcommand
+        .setName("lbs-to-kg")
+        .setDescription("convert lbs to kg")
+        .addNumberOption((option) =>
+          option.setName("lbs").setDescription("lbs").setRequired(true)
+        )
+    ),
+
+  async execute(interaction) {
+    const subCommand: string = interaction.options.getSubcommand();
+
+    switch (subCommand) {
+      case "celsius-to-fehrenheit": {
+        const celsiusToFehrenheit = await import(
+          "../sub-commands/conversion/celsius-to-fehrenheit.js"
+        );
+        celsiusToFehrenheit.subCommand(interaction);
+        break;
+      }
+      case "fehrenheit-to-celsius": {
+        break;
+      }
+      case "kg-to-lbs": {
+        break;
+      }
+      case "lbs-to-kg": {
+        break;
+      }
+    }
+  },
+};
