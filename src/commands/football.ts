3aef25af5c7e34bbce44a95e09a3db1c4cdfc8a7
Leighton Guang <leighton.guang@icloud.com>
2024-05-03 22:08:57 +0100

    refactor: move league-fixtures to a seperate file

--- src/commands/football.ts
+++ src/commands/football.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import axios from "axios";
 
 export const data = {
@@ -7,32 +7,40 @@ export const data = {
     .setDescription("sends a random football image")
     .addSubcommand((addSubCommand) =>
       addSubCommand
-        .setName("team")
-        .setDescription("sends a random football image for a team")
+        .setName("league-fixtures")
+        .setDescription("get upcoming fixtures from a legue")
+        .addStringOption((option) =>
+          option
+            .setName("league")
+            .setDescription("Select a league")
+            .setRequired(true)
+            .setChoices(
+              { name: "Champions League", value: "CL" },
+              { name: "Primeira Liga", value: "PPL" },
+              { name: "Premier League", value: "PL" },
+              { name: "Eredivisie", value: "DED" },
+              { name: "Bundesliga", value: "BL1" },
+              { name: "Ligue 1", value: "FL1" },
+              { name: "Serie A", value: "SA" },
+              { name: "La Liga", value: "PD" },
+              { name: "EFL Championship", value: "ELC" },
+              { name: "Campeonato Brasileiro Série A", value: "BSA" },
+              { name: "Euro 2024", value: "EC" }
+            )
+        )
     ),
 
   async execute(interaction) {
-    const FOOTBALLDATA_API_KEY = process.env.FOOTBALLDATA_API_KEY;
+    const subCommand: string = interaction.options.getSubcommand();
 
-    const footballDataApiUrl = "https://api.football-data.org/v4";
-
-    const headers = {
-      "X-Auth-Token": FOOTBALLDATA_API_KEY,
-    };
-
-    const {
-      data: { competition, matches },
-    }: { data: any; competitions: any; matches: any } = await axios.get(
-      footballDataApiUrl + "/competitions/CL/matches",
-      {
-        headers,
+    switch (subCommand) {
+      case "league-fixtures": {
+        const leagueFixtures = await import(
+          "../sub-commands/football/league-fixtures.js"
+        );
+        leagueFixtures.subCommnand(interaction);
+        break;
       }
-    );
-
-    const lastTenMatches: any[] = matches.slice(-10);
-
-    console.log(lastTenMatches);
-
-    await interaction.reply({ content: "/football" });
+    }
   },
 };
