d777671011b27a2f8e057b6173ebdf76da53be58
Leighton Guang <leighton.guang@icloud.com>
2024-06-15 21:21:29 +0100

    feat: new command to show football result

--- src/commands/football.ts
+++ src/commands/football.ts
@@ -8,7 +8,31 @@ export const data = {
     .addSubcommand((addSubCommand) =>
       addSubCommand
         .setName("league-fixtures")
-        .setDescription("get upcoming fixtures from a legue")
+        .setDescription("get upcoming fixtures from a league")
+        .addStringOption((option) =>
+          option
+            .setName("league")
+            .setDescription("Select a league")
+            .setRequired(true)
+            .setChoices(
+              { name: "Champions League", value: "CL" },
+              { name: "Primeira Liga", value: "PPL" },
+              { name: "Premier League", value: "PL" },
+              { name: "Eredivisie", value: "DED" },
+              { name: "Bundesliga", value: "BL1" },
+              { name: "Ligue 1", value: "FL1" },
+              { name: "Serie A", value: "SA" },
+              { name: "La Liga", value: "PD" },
+              { name: "EFL Championship", value: "ELC" },
+              { name: "Campeonato Brasileiro Série A", value: "BSA" },
+              { name: "Euro 2024", value: "EC" }
+            )
+        )
+    )
+    .addSubcommand((addSubCommand) =>
+      addSubCommand
+        .setName("results")
+        .setDescription("get fixtures results from a league")
         .addStringOption((option) =>
           option
             .setName("league")
@@ -31,14 +55,26 @@ export const data = {
     ),
 
   async execute(interaction) {
+    const FOOTBALLDATA_API_KEY = process.env.FOOTBALLDATA_API_KEY;
+    const footballDataApiUrl = "https://api.football-data.org/v4";
+    const headers = {
+      "X-Auth-Token": FOOTBALLDATA_API_KEY,
+    };
+
     const subCommand: string = interaction.options.getSubcommand();
 
     switch (subCommand) {
       case "league-fixtures": {
-        const leagueFixtures = await import(
+        const leagueFixtures: any = await import(
           "../sub-commands/football/league-fixtures.js"
         );
-        leagueFixtures.subCommnand(interaction);
+        leagueFixtures.subCommnand(interaction, footballDataApiUrl, headers);
+        break;
+      }
+
+      case "results": {
+        const results: any = await import("../sub-commands/football/result.js");
+        results.subCommand(interaction, footballDataApiUrl, headers);
         break;
       }
     }
---
+++ src/sub-commands/football/result.ts
@@ -0,0 +1,64 @@
+import { EmbedBuilder } from "discord.js";
+import axios from "axios";
+
+export const subCommand = async (interaction, footballDataApiUrl, headers) => {
+  const leagueValue: string = interaction.options.get("league").value;
+
+  try {
+    const {
+      data: { competition, matches },
+    }: { data: any; competitions: any; matches: any } = await axios.get(
+      footballDataApiUrl + "/competitions/" + leagueValue + "/matches",
+      { headers }
+    );
+
+    const finishedMatches: any[] = matches.filter((match: any) => {
+      return (
+        match.status === "FINISHED" &&
+        (match.homeTeam.name !== null || match.awayTeam.name !== null)
+      );
+    });
+
+    let finishedMatchesList: EmbedBuilder[] = [];
+
+    const footballHeaderEmbed = new EmbedBuilder()
+      .setAuthor({
+        name: competition.name,
+        iconURL: competition.emblem,
+      })
+      .setColor(0xffffff);
+
+    finishedMatches.forEach((match) => {
+      const matchEmbed: EmbedBuilder = new EmbedBuilder()
+        .setTitle(
+          `${match.group.replace(/_/g, " ")}    ${match.homeTeam.name} vs ${
+            match.awayTeam.name
+          }`
+        )
+        .setDescription(
+          "```" +
+            match.score.halfTime.home +
+            " - " +
+            match.score.halfTime.away +
+            "```"
+        );
+
+      finishedMatchesList.push(matchEmbed);
+    });
+
+    if (finishedMatchesList.length > 5) {
+      // if there are more than 5 matches get the last 5
+      finishedMatchesList = finishedMatchesList.slice(-5);
+    }
+
+    try {
+      await interaction.reply({
+        embeds: [footballHeaderEmbed, ...finishedMatchesList],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
