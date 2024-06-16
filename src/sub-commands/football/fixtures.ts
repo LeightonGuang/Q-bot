b9fbc14f3a2a340760d2c0999c300a53ccd16f44
Leighton Guang <leighton.guang@icloud.com>
2024-06-16 20:57:20 +0100

    fix: show paused game when its half time

--- src/sub-commands/football/fixtures.ts
+++ src/sub-commands/football/fixtures.ts
@@ -13,37 +13,15 @@ export const subCommnand = async (interaction, footballDataApiUrl, headers) => {
     );
 
     const footballHeaderEmbed = new EmbedBuilder()
-      .setTitle(competition.name)
-      .setThumbnail(competition.emblem)
+      .setAuthor({
+        name: competition.name,
+        iconURL: competition.emblem,
+      })
       .setColor(0xffffff);
-
-    let upcomingMatches: any[] = matches.filter((match: any) => {
-      return (
-        match.status === "TIMED" &&
-        (match.homeTeam.name !== null || match.awayTeam.name !== null)
-      );
-    });
-
-    upcomingMatches = upcomingMatches.slice(0, 5);
-
-    const upcomingMatchesList: EmbedBuilder[] = [];
-
-    upcomingMatches.forEach((match) => {
-      const utcDate: Date = new Date(match.utcDate);
-      const unixTimestamp: Date | number = Math.floor(utcDate.getTime() / 1000);
-
-      const matchEmbed: EmbedBuilder = new EmbedBuilder()
-        .setColor(0x00ff00)
-        .setTitle(match.group.replace(/_/g, " "))
-        .setTitle(`${match.homeTeam.name} vs ${match.awayTeam.name}`)
-        .setDescription(`<t:${unixTimestamp}:d> <t:${unixTimestamp}:t>`);
-
-      upcomingMatchesList.push(matchEmbed);
-    });
 
     const liveMatches: any[] = matches.filter((match: any) => {
       return (
-        match.status === "IN_PLAY" &&
+        (match.status === "IN_PLAY" || match.status === "PAUSED") &&
         (match.homeTeam.name !== null || match.awayTeam.name !== null)
       );
     });
@@ -61,12 +39,44 @@ export const subCommnand = async (interaction, footballDataApiUrl, headers) => {
       liveMatchesList.push(matchEmbed);
     });
 
+    let upcomingMatches: any[] = matches.filter((match: any) => {
+      return (
+        match.status === "TIMED" &&
+        (match.homeTeam.name !== null || match.awayTeam.name !== null)
+      );
+    });
+
+    if (upcomingMatches.length === 0) {
+      const noUpcomingMatches: EmbedBuilder = new EmbedBuilder()
+        .setColor(0xff0000)
+        .setTitle("No upcoming matches");
+    }
+
+    upcomingMatches = upcomingMatches.slice(0, 5);
+
+    const upcomingMatchesList: EmbedBuilder[] = [];
+
+    upcomingMatches.forEach((match) => {
+      const utcDate: Date = new Date(match.utcDate);
+      const unixTimestamp: Date | number = Math.floor(utcDate.getTime() / 1000);
+
+      const matchEmbed: EmbedBuilder = new EmbedBuilder()
+        .setColor(0x00ff00)
+        .setTitle(
+          match.group.replace(/_/g, " ") +
+            `  ${match.homeTeam.name} vs ${match.awayTeam.name}`
+        )
+        .setDescription(`<t:${unixTimestamp}:d> <t:${unixTimestamp}:t>`);
+
+      upcomingMatchesList.push(matchEmbed);
+    });
+
     try {
       await interaction.reply({
         embeds: [
           footballHeaderEmbed,
-          ...upcomingMatchesList,
           ...liveMatchesList,
+          ...upcomingMatchesList,
         ],
       });
     } catch (error) {
